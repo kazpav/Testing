d8e9c2cc45f083a9f1caae26ca790559ef41d32c
Valdemar <Valdemar@DESKTOP-BTL2ITH>
2017-03-05 12:39:44 +0200

    add some classes

---
+++ TestHub/src/ua/test/pack/class1.java
@@ -0,0 +1,6 @@
+package ua.test.pack;
+
+public class class1 {
+	private int number1;
+	private String string1 = "String1";
+}
---
+++ TestHub/src/ua/test/pack/class2.java
@@ -0,0 +1,5 @@
+package ua.test.pack;
+
+public class class2 {
+	private String ss = "hi";
+}
